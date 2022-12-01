ed14ffcb73168d46025630ae7559199813942c2f
QendresaZ <qz40092@ubt-uni.net>
2022-12-01 17:03:40 +0100

    Added index and standardPage templates

--- src/main/java/com/example/blogmanagementsystem/repository/UserRepository.java
+++ src/main/java/com/example/blogmanagementsystem/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.blogmanagementsystem.repository;
 
 import com.example.blogmanagementsystem.domain.User;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Lazy
 public interface UserRepository  extends ElasticsearchRepository<User, String> {
     User findByUsername(String username);
 }
