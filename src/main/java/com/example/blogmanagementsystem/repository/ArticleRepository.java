769b8b90e814ef239f1d356fcf9886a00032879e
QendresaZ <qz40092@ubt-uni.net>
2022-12-01 17:03:58 +0100

    fixes

--- src/main/java/com/example/blogmanagementsystem/repository/ArticleRepository.java
+++ src/main/java/com/example/blogmanagementsystem/repository/ArticleRepository.java
@@ -1,12 +1,16 @@
 package com.example.blogmanagementsystem.repository;
 
 import com.example.blogmanagementsystem.domain.Article;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
+@Lazy
 public interface ArticleRepository extends ElasticsearchRepository<Article, String> {
     Optional<Article> findByLink(String link);
     Page<Article> findByTitleContainingAndBodyContaining(String title, String body, Pageable pageable);
